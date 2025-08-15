3e42aa83ee99e8e53ad54c4f08baf73a4c32c1c2
Mohamed <mezomanar.mm@gmail.com>
2025-08-15 15:38:42 +0300

    fixed a bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,35 @@
 
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:dio/dio.dart';
+import 'data/datasources/news_remote_data_source.dart';
+import 'data/repositories/news_repository_impl.dart';
+import 'domain/usecases/get_news_usecase.dart';
+import 'presentation/providers/news_provider.dart';
+import 'presentation/pages/news_list_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final dio = Dio();
+    final newsRemoteDataSource = NewsRemoteDataSourceImpl(dio);
+    final newsRepository = NewsRepositoryImpl(newsRemoteDataSource);
+    final getNewsUseCase = GetNewsUseCase(newsRepository);
+
+    return ChangeNotifierProvider(
+      create: (_) => NewsProvider(getNewsUseCase),
+      child: MaterialApp(
+        title: 'News App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3: true,
+        ),
+        home: NewsListPage(),
+      ),
+    );
+  }
+}
